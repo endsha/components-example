c66abf0e396159ee4f1da159662576e8c915d1a2
eNd_SHA <anh.ns.dev@gmail.com>
2024-09-29 23:49:35 +0700

    feat: implement stop watch screen

--- src/screens/StopWatch.tsx
+++ src/screens/StopWatch.tsx
@@ -1,34 +1,98 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DateTime } from 'luxon';
 
 import Button, { ButtonShape, ButtonType } from '@components/Button';
 
-import Layout, { SCREEN_WIDTH } from '@constants/Layout';
+import Layout from '@constants/Layout';
+import Typo from '@constants/Typo';
+import Themes from '@constants/Themes';
+
 export default function StopWatch(): JSX.Element {
+  const [time, setTime] = React.useState(0);
+  const [isStart, setIsStart] = React.useState(false);
+  const [lapList, setLapList] = React.useState<number[]>([]);
+
+  const timerInterval = React.useRef<NodeJS.Timeout>();
+
+  React.useEffect(() => {
+    if (isStart) {
+      timerInterval.current = setInterval(() => {
+        setTime(prev => prev + 16.3);
+      }, 10);
+    } else {
+      clearInterval(timerInterval.current);
+    }
+    return () => clearInterval(timerInterval.current);
+  }, [isStart]);
+
+  const handleStart = () => {
+    setIsStart(true);
+  };
+
+  const handleStop = () => {
+    setIsStart(false);
+  };
+
+  const handleReset = () => {
+    setTime(0);
+  };
+
+  const handleLap = () => {
+    if (time > 0) {
+      setLapList(prev => [...prev, time]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>StopWatch</Text>
+      <View style={Layout.flexRow.alignStart}>
+        <Text style={styles.time} adjustsFontSizeToFit>
+          {DateTime.now()
+            .set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
+            .plus(time)
+            .toFormat('HH:mm:ss,uu')}
+        </Text>
+      </View>
       <View style={{ height: 56 }} />
       <View style={{ ...Layout.flexRow.alignCenter, paddingHorizontal: 20 }}>
-        <Button
-          title="Start"
-          type={ButtonType.Default}
-          shape={ButtonShape.Default}
-          style={{ flex: 1 }}
-          onPress={() => {
-            console.log('Start');
-          }}
-        />
-        {/* <Button
-          title="Stop"
-          type={ButtonType.Default}
-          shape={ButtonShape.Default}
-          style={{ flex: 1 }}
-          onPress={() => {
-            console.log('Start');
-          }}
-        /> */}
+        {isStart && time > 0 ? (
+          <Button
+            title="Lap"
+            type={ButtonType.Outline}
+            shape={ButtonShape.Default}
+            style={{ flex: 1 }}
+            onPress={handleLap}
+          />
+        ) : (
+          <Button
+            title="Reset"
+            type={ButtonType.Outline}
+            shape={ButtonShape.Default}
+            style={{ flex: 1 }}
+            onPress={handleReset}
+          />
+        )}
+        <View style={{ width: 12 }} />
+        {!isStart ? (
+          <Button
+            title="Start"
+            type={ButtonType.Default}
+            shape={ButtonShape.Default}
+            style={{ flex: 1 }}
+            onPress={handleStart}
+          />
+        ) : (
+          <Button
+            title="Stop"
+            type={ButtonType.Default}
+            shape={ButtonShape.Default}
+            style={{ flex: 1 }}
+            onPress={handleStop}
+          />
+        )}
       </View>
+      {/* TODO: Implement Lap list */}
     </View>
   );
 }
@@ -37,5 +101,10 @@ const styles = StyleSheet.create({
   container: {
     ...Layout.flexCol.center,
     flex: 1,
+  },
+  time: {
+    ...Typo.heading1,
+    fontWeight: '500',
+    color: Themes.slate900,
   },
 });
